cda97c492bce775a535040dc5b5d8999e9775efc
Ahmed Khalil <aakhalil89@gmail.com>
2020-11-05 14:30:10 +0100

    Update class to be public

--- AKOTPView/AKOTPView.swift
+++ AKOTPView/AKOTPView.swift
@@ -3,17 +3,40 @@
 //  AKOTPView
 //
 //  Created by Ahmed Khalil on 05.11.20.
+//  Copyright © 2020 Ahmed Khalil Taher. All rights reserved.
 //
+
+//    MIT License
+//
+//    Copyright (c) 2020 Ahmed Khalil Taher
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 
 import UIKit
 
-public enum KeyboardType: Int {
+@objc public enum KeyboardType: Int {
     case numeric
     case alphabet
     case alphaNumeric
 }
 
-class AKOTPView: UIView {
+@objc public class AKOTPView: UIView {
     
     public var fieldsCount: Int = 4
     public var otpInputType: KeyboardType = .numeric
@@ -105,7 +128,7 @@ class AKOTPView: UIView {
 
 extension AKOTPView: UITextFieldDelegate {
     
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         let otpString = getOTPString()
         if otpString.count == 0 {
             if textField.tag == otpTextFields.first?.tag {
@@ -128,7 +151,7 @@ extension AKOTPView: UITextFieldDelegate {
         return false
     }
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
         // writing text
         if textField.text == "" && string.count == 1 {
